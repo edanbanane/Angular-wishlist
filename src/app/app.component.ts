8de394f645ed9ec95c01d4f64d0e0d481de51d57
Anne-Marie Aubin <aubin.anne-marie@courrier.uqam.ca>
2024-03-18 17:04:18 -0400

    2.8 - Listening changes ngModels values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,28 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
+  listFilter : String = '0';
+
   newWishText  = '';
 
   title = 'wishlist';
+
+  visibleItems : WishItem[] = this.items; // initialise with Item array
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText)); // add wish to items array
     this.newWishText = ''; // clear the textbox
     // important de setter un name propertie pour le input de form
+  }
+
+  filterChanged(value : any) {
+    if (value==0){
+      this.visibleItems = this.items;
+    } else if (value == 1) {
+      this.visibleItems = this.items.filter(item => !item.isComplete)
+    } else {
+      this.visibleItems = this.items.filter(item => item.isComplete)
+    }
   }
 
   toggleItem(item : WishItem) {
