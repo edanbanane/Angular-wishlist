ed7d4e519270216672f8f551971ce633105f9824
Anne-Marie Aubin <aubin.anne-marie@courrier.uqam.ca>
2024-03-18 19:05:14 -0400

    3.2 - Outputing info from a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,10 @@ export class AppComponent {
 
   listFilter : any = '0';
 
-  newWishText  = '';
-
   title = 'wishlist';
 
   get visibleItems() : WishItem[] {
     return this.items.filter(filters[this.listFilter]);
   }; // initialise with Item array
-
-  addNewWish() {
-    this.items.push(new WishItem(this.newWishText)); // add wish to items array
-    this.newWishText = ''; // clear the textbox
-    // important de setter un name propertie pour le input de form
-  }
 
 }
