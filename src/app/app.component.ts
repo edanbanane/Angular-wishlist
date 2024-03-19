d393ec16bbfa79d3b3a2c7977467a512b23142fe
Anne-Marie Aubin <aubin.anne-marie@courrier.uqam.ca>
2024-03-19 10:20:06 -0400

    3.3 - Init component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { WishItem } from '../shared/models/wishItem';
 
-const filters = [
-  (item : WishItem) => item,
-  (item : WishItem) => !item.isComplete,
-  (item : WishItem) => item.isComplete
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,12 +13,10 @@ export class AppComponent {
     new WishItem('Find grass that cuts itself')
   ];
 
-  listFilter : any = '0';
-
-  title = 'wishlist';
+  filter : any = () => {}; //Initialise comme une fonction vide
 
   get visibleItems() : WishItem[] {
-    return this.items.filter(filters[this.listFilter]);
+    return this.items.filter(this.filter);
   }; // initialise with Item array
 
 }
